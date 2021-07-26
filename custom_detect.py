6ba23093a0f1365f6284b3a30fcd024a2e7b3b33
langdaiduong <38464506+langdaiduong@users.noreply.github.com>
2021-07-26 17:10:16 +0700

    fix classes

--- custom_detect.py
+++ custom_detect.py
@@ -12,9 +12,9 @@ import numpy as np
 from core.config import cfg
 
 input_size = 416
-video_path = "./data/video/he.mp4"
+video_path = "./data/video/hand_up.mp4"
 
-saved_model_loaded = tf.saved_model.load('./checkpoints/yolov4-personhand-tiny-416', tags=[tag_constants.SERVING])
+saved_model_loaded = tf.saved_model.load('./checkpoints/yolov4-classroom-tiny-416', tags=[tag_constants.SERVING])
 infer = saved_model_loaded.signatures['serving_default']
 
 try:
@@ -31,13 +31,15 @@ def read_class_names(class_file_name):
 # 0: person, 1: hand
 def image_best(number_frame, minfame, classes = read_class_names(cfg.YOLO.CLASSES), allowed_classes=list(read_class_names(cfg.YOLO.CLASSES).values())):
     num_classes = len(classes)
+    #print(num_classes)
     #lưu số frame detect của môi class
     count_frame_class = [0]*num_classes
     #lưu độ chính xác detect của môi class
     max_score_class = [0]*num_classes
     #số frame hiện tại
     count_frame = 0
-    result_image = [[]]*num_classes
+    result_image = []
+    #print(result_image)
     class_arr = [[]]*num_classes
     #lưu độ chính xác cao nhất của một class trong tất cả các frame
     while count_frame < number_frame:
@@ -48,7 +50,7 @@ def image_best(number_frame, minfame, classes = read_class_names(cfg.YOLO.CLASSE
         else:
             print('Video has ended or failed, try a different video format!')
             break
-        if count_frame % 1 == 0:
+        if count_frame % 2 == 0:
             # width:480 height: 854 cua video
             # frame_size = frame.shape[:2]
             # print(frame_size)
@@ -70,8 +72,8 @@ def image_best(number_frame, minfame, classes = read_class_names(cfg.YOLO.CLASSE
                     pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
                 max_output_size_per_class=50,
                 max_total_size=50,
-                iou_threshold=0.25,
-                score_threshold=0.5
+                iou_threshold=0.3,
+                score_threshold=0.3
             )
             #print(boxes)
             pred_bbox = [boxes.numpy(), scores.numpy(), classes.numpy(), valid_detections.numpy()]
@@ -84,7 +86,7 @@ def image_best(number_frame, minfame, classes = read_class_names(cfg.YOLO.CLASSE
                 if pred_bbox[2][0][i] == i:
                     if pred_bbox[1][0][i] > 0.5:
                         count_frame_class[i]+=1
-                    if pred_bbox[1][0][i] >= max_score_class[i]:
+                    if pred_bbox[1][0][i] >= max_score_class[i] and pred_bbox[2][0][i] == i:
                         max_score_class[i] = pred_bbox[1][0][i]
                         class_arr[i].append([pred_bbox,image])
             fps = 1.0 / (time.time() - start_time)
@@ -101,42 +103,40 @@ def image_best(number_frame, minfame, classes = read_class_names(cfg.YOLO.CLASSE
             print(classes[i] + ":" + str(count_frame_class[i]))
     cv2.destroyAllWindows()
     ###############################################################################
+    #print(max_score_class)
     for z in range(num_classes):
         if count_frame_class[z] >= minfame[z]:
             for i in range(len(class_arr[z])):
-                if class_arr[z][i][0][1][0][z] == max_score_class[z] and z!=0:
-                    result_image[z].append(class_arr[z][i])
-    # z=1
-    # if count_frame_class[z] >= minfame[z]:
-    #     for i in range(len(class_arr[z])):
-    #         if class_arr[z][i][0][1][0][z] == max_score_class[z]:
-    #             result_image[z].append(class_arr[z][i])
+                if class_arr[z][i][0][1][0][z] == max_score_class[z] and class_arr[z][i][0][2][0][z] == z:
+                    #print("check",class_arr[z][i])
+                    result_image.append(class_arr[z][i])
+                    #print(z, "->", result_image[z], "->",class_arr[z][i][0][2][0][z])
 
     classes = read_class_names(cfg.YOLO.CLASSES)
     max_score = [0]*num_classes
     result = []
     for i in range(num_classes):
-        if result_image[i] != [] and count_frame_class[i] >= minfame[i]:
-            if result_image[i][0][0][1][0][i] >= max_score[i] and result_image[i][0][0][2][0][i]==i:
-                max_score[i] = result_image[i][0][0][1][0][i]
+        if count_frame_class[i] >= minfame[i]:
+            print(i, "->", result_image[i][0][1][0][i] )
+            if result_image[i][0][1][0][i] >= max_score[i] and result_image[i][0][2][0][i]==i:
+                max_score[i] = result_image[i][0][1][0][i]
     #print(max_score)
     for i in range(num_classes):    
-        if result_image[i] != [] and count_frame_class[i] >= minfame[i]:
-            # print(i)
-            if result_image[i][0][0][1][0][i] == max_score[i]:
-                # print("hihi")
-                # print(result_image[i][0][0][2][0], "->",i)
-                if result_image[i][0][0][2][0][i] == i and count_frame_class[i] >= minfame[i]:
-                    arr_result = [[classes[result_image[i][0][0][2][0][i]]],result_image[i][0][0][0][0][i],result_image[i][0][0][1][0][i]]
+        if count_frame_class[i] >= minfame[i]:
+            #print(max_score)
+            if result_image[i][0][1][0][i] == max_score[i]:
+                #print(result_image[i][0][0][1][0][i], "->",i)
+                if result_image[i][0][2][0][i] == i:
+                    arr_result = [[classes[result_image[i][0][2][0][i]]],result_image[i][0][0][0][i],result_image[i][0][1][0][i]]
                     result.append(arr_result)
                     #print("result", result)
-                    if result_image[i][0][1] is not None:
-                        image = Image.fromarray(result_image[i][0][1].astype(np.uint8))
-                        image = cv2.cvtColor(np.array(result_image[i][0][1]), cv2.COLOR_BGR2RGB)
+                    if result_image[i][1] is not None:
+                        image = Image.fromarray(result_image[i][1].astype(np.uint8))
+                        image = cv2.cvtColor(np.array(result_image[i][1]), cv2.COLOR_BGR2RGB)
                         cv2.imwrite('./detections/detection'+ str(i) +'.png', image)
     return result
 #image_best(number frame,[min frame class 0, min frame class 1])               
-result = image_best(50, [10, 10])
+result = image_best(50, [10, 10, 1])
 if result != []:
     print(result)
 else: 
